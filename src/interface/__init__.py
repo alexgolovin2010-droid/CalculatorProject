453d0f1c1eeedb23b80632ca4c1e16745d482a6a
Timurkaaa <timurzarovskiy@gmail.com>
2025-10-05 15:34:53 +0300

    feat(menu): update __init__

--- src/interface/__init__.py
+++ src/interface/__init__.py
@@ -1,5 +1,7 @@
 from .main_menu import main_menu
+from .menu import menu
+
 from .input import custom_input as input
 from .output import custom_output as output
 
-__all__ = ["main_menu", "input", "output"]
+__all__ = ["menu", "input", "output"]
