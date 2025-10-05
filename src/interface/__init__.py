e6aaf4c5b4e8baf13f30adf24976b7ec17b363a7
Timurkaaa <timurzarovskiy@gmail.com>
2025-10-05 15:11:30 +0300

    feat(output): create custom output function

--- src/interface/__init__.py
+++ src/interface/__init__.py
@@ -1,3 +1,5 @@
 from .main_menu import main_menu
+from .input import custom_input as input
+from .output import custom_output as output
 
-__all__ = ["main_menu"]
+__all__ = ["main_menu", "input", "output"]
