d2a62f22532a4e6916ca115acad4c85472e512ff
Timurkaaa <timurzarovskiy@gmail.com>
2025-10-05 15:10:25 +0300

    feat(output): create custom output function

--- src/interface/output.py
+++ src/interface/output.py
@@ -1,4 +1,5 @@
 from .utils import Format
 
 def custom_output(text: str) -> None:
+    """Custom output function for project"""
     print(Format.Style.bold(Format.Color.green("Результат: ")) + str(text))
