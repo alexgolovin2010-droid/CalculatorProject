1a75b52f5f1e11f430d9a1d9feeea3c79fe94da1
Timurkaaa <timurzarovskiy@gmail.com>
2025-10-06 19:25:12 +0300

    feat(localization): update logic function for using locales

--- src/functions/logic.py
+++ src/functions/logic.py
@@ -1,23 +1,24 @@
 import interface
+from localization import loc
 
 def calc_logic() -> bool:
     """TASK 5: Function for logical calculations"""
     interface.menu(["and", "or", "not"])
-    choice = interface.input("Выберите действие: ", type=int, options=[1, 2, 3])
+    choice = interface.input(loc("action_input"), type=int, options=[1, 2, 3])
 
     match choice:
         case 1:
-            a = interface.input("Введите первое число: ", type=bool)
-            b = interface.input("Введите второе число: ", type=bool)
+            a = interface.input(loc("first_num_input"), type=bool)
+            b = interface.input(loc("second_num_input"), type=bool)
 
             return (a and b)
         case 2:
-            a = interface.input("Введите первое число: ", type=bool)
-            b = interface.input("Введите второе число: ", type=bool)
+            a = interface.input(loc("first_num_input"), type=bool)
+            b = interface.input(loc("second_num_input"), type=bool)
 
             return (a or b)
         case 3:
-            a = interface.input("Введите число: ", type=bool)
+            a = interface.input(loc("num_input"), type=bool)
             return (not a)
 
 
